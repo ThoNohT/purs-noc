7ffc3e34e4de76a1a3cc276952adc995cba2562e
ThoNohT <e.c.p.bataille@gmail.com>
2020-04-19 16:41:30 +0200

    First version of the mouse vectors from video 1.5

---
+++ src/Apps/NatureOfCode/1.5-MouseVectors.purs
@@ -0,0 +1,60 @@
+-- | Implements the vectors pointing at the mouse from https://www.youtube.com/watch?v=ttz05d8DSOs.
+module Apps.NatureOfCode.MouseVectors (app) where
+
+import Prelude
+
+import App as App
+import Data.Maybe (Maybe(..))
+import Effect (Effect)
+import Graphics (background, line, resetTransform)
+import Graphics.Canvas as GC
+import Model.Events (MouseEvent(..), MouseData)
+import Model.Vector (Vector2, (<=>))
+
+-- | The state just contains the location of the mouse.
+type State
+  = Vector2
+
+-- | This value is used to set and get the canvas size everywhere  else.
+canvasSize :: GC.Dimensions
+canvasSize = { width: 400.0, height: 400.0 }
+
+-- | Sets the canvas to the desired size.
+initialize :: GC.CanvasElement -> State -> Effect (Maybe State)
+initialize canvas _ = do
+  _ <- GC.setCanvasDimensions canvas canvasSize
+  _ <- background canvas "black"
+  pure Nothing
+
+tick :: State -> Effect (Maybe State)
+tick state = do
+  pure Nothing
+
+handleMouse :: MouseData -> State -> Effect (Maybe State)
+handleMouse mouse state = case mouse.event of
+  MouseMove -> pure $ Just mouse.location
+  _ -> pure Nothing
+
+-- | Renders a white background, and a red square around the mouse position.
+render :: GC.Context2D -> State -> Effect Unit
+render ctx state = do
+  _ <- resetTransform ctx
+  _ <- GC.translate ctx { translateX: canvasSize.width / 2.0, translateY: canvasSize.height / 2.0 }
+
+  let v = state - (200.0 <=> 200.0)
+
+  _ <- GC.setStrokeStyle ctx "#FFFFFF64"
+  _ <- GC.setLineWidth ctx 2.0
+  line ctx zero v
+
+-- | Define the main application.
+app :: App.CanvasApp
+app =
+  App.app
+    $ (App.defaultAppSpec zero)
+        { initialize = initialize
+        , render = render
+        , tick = tick
+        , updateInterval = 33
+        , handleMouse = handleMouse
+        }
--- src/Main.purs
+++ src/Main.purs
@@ -7,7 +7,7 @@ import Halogen.VDom.Driver (runUI)
 {-
     Import a specific app here as ActiveApp to run this app.
  -}
-import Apps.NatureOfCode.RandomWalker5 as ActiveApp
+import Apps.NatureOfCode.MouseVectors as ActiveApp
 
 main :: Effect Unit
 main =
