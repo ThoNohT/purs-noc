609d5a540a2ce4660febda664fb0bc69c5a553f4
ThoNohT <e.c.p.bataille@gmail.com>
2020-04-25 22:04:22 +0200

    Add conversion between degrees and radians

--- src/Model/Math.purs
+++ src/Model/Math.purs
@@ -1,4 +1,4 @@
-module Model.Math (constrain) where
+module Model.Math (constrain, degrees, radians) where
 
 import Prelude
 import Math as Math
@@ -9,3 +9,11 @@ constrain min max number =
   number
     # Math.min max
     # Math.max min
+
+-- | Converts degrees into radians.
+radians :: Number -> Number
+radians deg = (deg / 360.0) * Math.pi * 2.0
+
+-- | Converts radians into degrees.
+degrees :: Number -> Number
+degrees rad = 360.0 * rad / (Math.pi * 2.0)
